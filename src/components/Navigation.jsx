dec778ef7da7159db89a3a73ca5316d70ffa6bf1
Mohamed Meshaima <mohamedoaderu@gmail.com>
2025-08-17 15:10:45 +0300

    Update links

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -14,7 +14,7 @@ const Navigation = () => {
                     <li><Link to="/home">Home</Link></li>
                     <li><Link to="/for-you">For You</Link></li>
                     <li><Link to="/">Watchlist</Link></li>
-                    <li><Link to="/">About Us (we’re broke)</Link></li>
+                    <li><Link to="/">About Us (we’re broke..)</Link></li>
                 </ul>
             </div>
         </nav>
