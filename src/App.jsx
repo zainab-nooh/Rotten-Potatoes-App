0238ce95cd271e56157ac600fd4ac86887428861
Mohamed Meshaima <mohamedoaderu@gmail.com>
2025-08-17 12:58:23 +0300

    inital app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-
+import Footer from './components/Footer'
 import './App.css'
 
 function App() {
@@ -8,6 +8,7 @@ function App() {
   return (
     <>
       <h1>Hello world</h1>
+      <Footer />
     </>
   )
 }
